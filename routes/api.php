7e6c282c12e822436e449cd902255e7441c80c7f
larryshow40 <larryshow40@yahoo.com>
2021-10-13 14:40:20 +0100

    updte on plan

--- routes/api.php
+++ routes/api.php
@@ -25,5 +25,5 @@ Route::post('dislike-code', 'ApiController@dislikeCode');
 Route::post('store-comment', 'ApiController@storeComment');
 Route::get('list-codes', 'ApiController@listCodes');
 Route::post('paystack/webhook', 'ApiController@paystackWebhook');
-Route::post('paystack/callback', 'ApiController@paystackCallback');
+Route::get('paystack/callback', 'ApiController@paystackCallback');
 
