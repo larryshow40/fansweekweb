ff0c58062050743b7fda0536af66c076e382632f
Lolu Adelore <adeloreisaac@gmail.com>
2022-07-13 17:33:09 +0100

    Fix

--- resources/views/site/partials/home/betting-code.blade.php
+++ resources/views/site/partials/home/betting-code.blade.php
@@ -2,14 +2,20 @@
     .row {
         font-size: 15px;
     }
-
 </style>
 <div class="sg-widget">
-    <h3 style="font-weight:bolder; font-size:19px;  color:white;" class="widget-title bg-danger">Today's Betting Codes
+    <h3 style="font-weight:bolder; font-size:19px;  color:white;" class="widget-title mb-0 bg-danger">Today's Betting
+        Codes
     </h3>
-    <div class="card-body py-0">
-        <div class="text-right">
-            <button type="button" class="btn btn-secondary mb-4" data-toggle="modal" data-target="#exampleModal">
+    <div class="card-body px-0 pb-0 pt-1">
+        <div class="d-flex justify-content-between">
+            @php
+            $mytime = Carbon\Carbon::now();
+            @endphp
+            <div>
+                {{ $mytime->format('l jS F Y ') }}
+            </div>
+            <button type="button" class="btn btn-sm btn-success mb-4" data-toggle="modal" data-target="#exampleModal">
                 Share Code
             </button>
         </div>
@@ -30,49 +36,48 @@
         </div>
     </div>
     @foreach ($codeGroups as $key => $group)
-        <div class="section-title my-2">
-            <h6 class="text-danger">{{$key}}</h6>
-        </div>
+    {{-- <div class="section-title my-2">
+        <h6 class="text-danger">{{$key}}</h6>
+    </div> --}}
     @foreach ($group as $code)
-        <div class="card bg-dark mb-1" style="color:white;">
-            <div class="card-body py-2">
-                <div class="row">
-                    <div style="font-weight: bolder; overflow:hidden; text-overflow:ellipsis; white-space: nowrap;"
-                        class="col-4">{{ $code->name }}<br /></div>
-                    <div class="col-5">
-                        <a style="text-decoration: none; color:lightblue;"
-                            href="{{ route('code.show', $code->id) }}">{{ $code->code }}<br /><i
-                                style="color: white;">{{ $code->user->first_name }}</i></a>
-                    </div>
-                    <div class="col-3">
-                        @if (Sentinel::check())
-                            <button type="button"
-                                class="btn btn-secondary likebutton {{ $code->likes->where('user_id', Sentinel::getUser()->id)->first() ? 'clicked' : '' }}"
-                                id="like{{ $code->id }}"><i
-                                    class="fa fa-thumbs-up"></i>&nbsp;<span>{{ $code->likes->count() }}</span></button>
-                            <button type="button"
-                                class="btn btn-secondary dislikebutton {{ $code->dislikes->where('user_id', Sentinel::getUser()->id)->first() ? 'clicked' : '' }}"
-                                id="dislike{{ $code->id }}"><i
-                                    class="fa fa-thumbs-down"></i>&nbsp;<span>{{ $code->dislikes->count() }}</span></button>
-                        @else
-                            <a href="{{ route('site.login.form') }}"
-                                onclick="return confirm('Please login to proceed with this action')"
-                                class="badge badge-secondary">
-                                <i class="fa fa-thumbs-up"></i>&nbsp;<span>{{ $code->likes->count() }}</span>
-                            </a>
-                            <a href="{{ route('site.login.form') }}"
-                                onclick="return confirm('Please login to proceed with this action')"
-                                class="badge badge-secondary">
-                                <i class="fa fa-thumbs-down"></i>&nbsp;<span>{{ $code->dislikes->count() }}</span>
-                            </a>
-
-
-                        @endif
-                    </div>
+    <div class="card bg-dark mb-1" style="color:white;">
+        <div class="card-body py-2">
+            <div class="row">
+                <div style="font-weight: bolder; overflow:hidden; text-overflow:ellipsis; white-space: nowrap;"
+                    class="col-4">{{ $code->name }}<br /></div>
+                <div class="col-4">
+                    <a style="text-decoration: none; color:lightblue;" href="{{ route('code.show', $code->id) }}">{{
+                        $code->code }}<br /><i style="color: white;">{{ $code->user->first_name }}</i></a>
                 </div>
+                <div class="col-4 px-1">
+                    @if (Sentinel::check())
+                    <button type="button"
+                        class="btn btn-sm btn-secondary likebutton {{ $code->likes->where('user_id', Sentinel::getUser()->id)->first() ? 'clicked' : '' }}"
+                        id="like{{ $code->id }}"><i class="fa fa-thumbs-up"></i>&nbsp;<span>{{ $code->likes->count()
+                            }}</span></button>
+                    <button type="button"
+                        class="btn btn-sm btn-secondary dislikebutton {{ $code->dislikes->where('user_id', Sentinel::getUser()->id)->first() ? 'clicked' : '' }}"
+                        id="dislike{{ $code->id }}"><i class="fa fa-thumbs-down"></i>&nbsp;<span>{{
+                            $code->dislikes->count() }}</span></button>
+                    @else
+                    <a href="{{ route('site.login.form') }}"
+                        onclick="return confirm('Please login to proceed with this action')"
+                        class="badge badge-secondary">
+                        <i class="fa fa-thumbs-up"></i>&nbsp;<span>{{ $code->likes->count() }}</span>
+                    </a>
+                    <a href="{{ route('site.login.form') }}"
+                        onclick="return confirm('Please login to proceed with this action')"
+                        class="badge badge-secondary">
+                        <i class="fa fa-thumbs-down"></i>&nbsp;<span>{{ $code->dislikes->count() }}</span>
+                    </a>
 
+
+                    @endif
+                </div>
             </div>
+
         </div>
+    </div>
     @endforeach
     @endforeach
     <a href="{{ route('all-codes') }}" class="btn btn-secondary btn-block">View All</a>
--- resources/views/site/partials/home/primary/style_1.blade.php
+++ resources/views/site/partials/home/primary/style_1.blade.php
@@ -21,7 +21,7 @@ $blockPosts = $posts->take(4);
 <div class="sg-home-section">
     <div class="container">
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-8">
                 <div class="post-slider">
                     @foreach ($sliderPosts as $post)
                         <div class="sg-post featured-post">
@@ -56,10 +56,13 @@ $blockPosts = $posts->take(4);
                     @endforeach
                 </div>
             </div>
-            <div class="col-lg-6">
+            <div class="col-lg-4">
                 <div class="row">
+                    <div class="col-12">
+                        @include('site.partials.home.betting-code')
+                    </div>
                     {{-- @php dd($blockPosts); @endphp --}}
-                    @foreach ($blockPosts as $post)
+                    {{-- @foreach ($blockPosts as $post)
                         <div class="col-md-6">
                             <div class="sg-post">
                                 <div class="entry-header">
@@ -112,11 +115,66 @@ $blockPosts = $posts->take(4);
 
                             </div>
                         </div>
-                    @endforeach
+                    @endforeach --}}
                 </div>
                 <div class="row">
-                    <div class="col-12 d-md-none d-lg-none d-xl-none d-block">
-                        @include('site.partials.home.betting-code')
+                    <div class="col-12 d-md-block d-lg-block d-xl-block d-none">
+                        {{-- @include('site.partials.home.betting-code') --}}
+                        {{-- @php dd($blockPosts); @endphp --}}
+                        {{-- @foreach ($blockPosts as $post)
+                            <div class="col-12">
+                                <div class="sg-post">
+                                    <div class="entry-header">
+                                        <div class="entry-thumbnail">
+                                            <a href="{{ route('article.detail', ['id' => $post->slug]) }}">
+                                                @if (isFileExist(@$post['image'], $result = @$post['image']->medium_image))
+                                                    <img src="{{ safari_check() ? basePath(@$post['image']) . '/' . $result : static_asset('default-image/default-358x215.png') }} "
+                                                        data-original=" {{ basePath(@$post['image']) }}/{{ $result }} "
+                                                        class="img-fluid lazy" alt="{!! $post->title !!}">
+                                                @else
+                                                    <img src="{{ static_asset('default-image/default-358x215.png') }} "
+                                                        class="img-fluid" alt="{!! $post->title !!}">
+                                                @endif
+                                            </a>
+                                        </div>
+                                        @if ($post->post_type == 'video')
+                                            <div class="video-icon block">
+                                                <img src="{{ static_asset('default-image/video-icon.svg') }} "
+                                                    alt="video-icon">
+                                            </div>
+                                        @elseif($post->post_type=="audio")
+                                            <div class="video-icon block">
+                                                <img src="{{ static_asset('default-image/audio-icon.svg') }} "
+                                                    alt="audio-icon">
+                                            </div>
+                                        @endif
+                                        <div class="category">
+                                            <ul class="global-list">
+                                                @isset($post->category->category_name)
+                                                    <li>
+                                                        <a
+                                                            href="{{ url('category', $post['category']->slug) }}">{{ $post['category']->category_name }}</a>
+                                                    </li>
+                                                @endisset
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="entry-content block">
+                                        <a href="{{ route('article.detail', ['id' => $post->slug]) }}">
+                                            <p>{!! \Illuminate\Support\Str::limit($post->title, 40) !!}</p>
+                                        </a>
+                                        <div class="entry-meta">
+                                            <ul class="global-list">
+                                                <li><a
+                                                        href="{{ route('article.date', date('Y-m-d', strtotime($post->updated_at))) }}">
+                                                        {{ $post->updated_at->format('F j, Y') }}</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        @endforeach --}}
 
                     </div>
                 </div>
@@ -154,7 +212,7 @@ $blockPosts = $posts->take(4);
                                 <input type="submit" name="yesterday" value="Yesterday" class="btn {{request()->yesterday ? 'btn-danger' : 'btn-secondary'}}">
                                 <input type="submit" name="today" value="Today" class="btn {{(!request()->tomorrow && !request()->yesterday) ? 'btn-danger' : 'btn-secondary'}}">
                                 <input type="submit" name="tomorrow" value="Tomorrow" class="btn {{request()->tomorrow ? 'btn-danger' : 'btn-secondary'}}">
-                                
+
                             </div>
 
                         </form>
@@ -229,7 +287,8 @@ $blockPosts = $posts->take(4);
             </div>
             <div class="col-md-5 col-lg-4 sg-sticky">
                 <div class="sg-sidebar theiaStickySidebar">
-                    <div class="d-md-block d-lg-block d-xl-block d-none">@include('site.partials.home.betting-code')
+                    <div class="d-md-block d-lg-block d-xl-block d-none">
+                        {{-- @include('site.partials.home.betting-code') --}}
                     </div>
                     @include('site.partials.right_sidebar_widgets')
                 </div>
